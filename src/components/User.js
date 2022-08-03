064c001fd4427ac77315bce04f816f6486e9ec32
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-08-03 18:02:56 +0600

    added a function for displaying first and last nam

--- src/components/User.js
+++ src/components/User.js
@@ -1,37 +1,47 @@
-import React from 'react'
-import FirstName from './FirstName'
-import LastName from './LastName'
+import React from "react";
+import { useDispatch } from "react-redux";
+import FirstName from "./FirstName";
+import LastName from "./LastName";
+import { setFirstName, setLastName } from "../features/user/userSlice";
 
 const User = () => {
-    return (
-        <div className='flex flex-col'>
-            <input
-                type='text'
-                placeholder='First Name'
-                className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
-            />
-            <input
-                type='text'
-                placeholder='Second Name'
-                className='w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
-            />
-            <div className='flex gap-20 py-5'>
-                <div className='flex flex-col'>
-                    <div className='flex font-light'>First Name:</div>
-                    <div className='flex'>
-                        <FirstName />
-                    </div>
-                </div>
+  const dispatch = useDispatch();
 
-                <div className='flex flex-col'>
-                    <div className='flex font-light'>Last Name:</div>
-                    <div className='flex'>
-                        <LastName />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col">
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={(e) => {
+          dispatch(setFirstName(e.target.value));
+        }}
+        className="w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm"
+      />
+      <input
+        type="text"
+        placeholder="Second Name"
+        onChange={(e) => {
+          dispatch(setLastName(e.target.value));
+        }}
+        className="w-full p-1 mb-2 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm"
+      />
+      <div className="flex gap-20 py-5">
+        <div className="flex flex-col">
+          <div className="flex font-light">First Name:</div>
+          <div className="flex">
+            <FirstName />
+          </div>
         </div>
-    )
-}
 
-export default User
+        <div className="flex flex-col">
+          <div className="flex font-light">Last Name:</div>
+          <div className="flex">
+            <LastName />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
