1e1ed654e42002e14b41b5671f3c500f60ce8909
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-08-03 23:50:35 +0600

    added save in localStorage via redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import userSlice from "../features/user/userSlice";
 
-export const store = configureStore({
-  reducer: {
-    user: userSlice,
-  },
+const rootReducer = combineReducers({
+  user: userSlice,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export default store;
