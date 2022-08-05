a46b9662e8c87fc9c1b8ae0e6a2470a2e0792631
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-08-05 11:50:38 +0600

    added complete function

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleCompletedTodo } from "../features/todo/todoSlice";
 
 const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
+
+  const toggleTodoHandler = (id) => {
+    dispatch(toggleCompletedTodo(id));
+  };
   return (
     <div className="flex justify-between items-center my-2">
-      <div className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400">
+      <div
+        className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400"
+        onClick={() => toggleTodoHandler(todo.id)}
+      >
         Complete
       </div>
       <div
