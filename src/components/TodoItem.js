89bf2aa415bb6e70fae364eeb4e7c734b775b360
Adilet Berdibekov <40534332+berdibekovadilet@users.noreply.github.com>
2022-08-05 11:55:57 +0600

    added remove function todo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { toggleCompletedTodo } from "../features/todo/todoSlice";
+import { toggleCompletedTodo, removeTodo } from "../features/todo/todoSlice";
 
 const TodoItem = ({ todo }) => {
   const dispatch = useDispatch();
@@ -8,11 +8,16 @@ const TodoItem = ({ todo }) => {
   const toggleTodoHandler = (id) => {
     dispatch(toggleCompletedTodo(id));
   };
+
+  const removeTodoHandler = (id) => {
+    dispatch(removeTodo(id));
+  };
+
   return (
     <div className="flex justify-between items-center my-2">
       <div
-        className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400"
         onClick={() => toggleTodoHandler(todo.id)}
+        className="text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400"
       >
         Complete
       </div>
@@ -23,7 +28,10 @@ const TodoItem = ({ todo }) => {
       >
         {todo.text}
       </div>
-      <div className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer">
+      <div
+        onClick={() => removeTodoHandler(todo.id)}
+        className="text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer"
+      >
         Delete
       </div>
     </div>
